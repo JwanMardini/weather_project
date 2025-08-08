638816f2fbb2f4a805f79d86c9e1cc546a83fb81
JwanMardini <jwan.99.mardini@gmail.com>
2025-08-08 15:17:14 +0200

    Implement connectivity checks and update dependencies for network status handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,66 @@
+
 import 'package:flutter/material.dart';
 import 'package:weather_project/router/app_router.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool _isConnected = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    Connectivity().onConnectivityChanged.listen((result) {
+      setState(() {
+        _isConnected = result != ConnectivityResult.none;
+      });
+    });
+  }
+
+  Future<void> _checkConnectivity() async {
+    var result = await Connectivity().checkConnectivity();
+    setState(() {
+      _isConnected = result != ConnectivityResult.none;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: appRouter,
+      builder: (context, child) {
+        if (!_isConnected) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.wifi_off, size: 64, color: Colors.red),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'No internet connection',
+                    style: TextStyle(fontSize: 20, color: Colors.red),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        return child!;
+      },
     );
   }
 }
